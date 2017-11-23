6e4c10fc39979ef64cd027cc9f6ee88e5caaa556
Tom Straszewski <tomek.straszewski@gmail.com>
2017-11-23 01:41:05 +0000

    implemented all base features

---
+++ js/quotes.js
@@ -0,0 +1,59 @@
+
+var quotes = [];
+
+$.get( "http://localhost:3000/data/quotes.txt", function (data, status) {
+    
+    var allQuotes = String(data);
+    
+    var index = 0;
+    var line = "";
+    var singleQuote = "";
+    var maxIndex = allQuotes.length;
+    allQuotes = allQuotes.replace(/\n/g,'£');
+   // allQuotes = allQuotes.replace(/-{2}/g,'¬');
+    
+            
+            
+    while( index < maxIndex){
+        line = "";
+        while(allQuotes[index] != '£'){
+            line += allQuotes[index];
+            index++;
+            if(index >= maxIndex) break;
+        }
+
+        singleQuote += line;          //add line to quote
+        if ( line.length < 2 ){
+            //new quote
+            quotes.push(singleQuote);
+            singleQuote = "";
+        }
+        index++;
+        if(index >= maxIndex) break;
+    }
+    
+    // only update content when data is parsed ! 
+    quoteUpdate();
+
+    $().ready( function () {
+        
+            $('button').text("Next Quote");
+            $('button').on('click',quoteUpdate);
+    });
+        
+});
+
+function quoteUpdate() {
+    
+        var max_index = quotes.length-1;
+        var max = max_index;
+        var min = 0;
+        var index = Math.ceil(Math.random() * (max - min) + min);
+    
+        var qAndAuthor = quotes[index].replace(/-{2}/,"¬");
+        var qAndAuthorArr = qAndAuthor.split("¬");
+        $('#quote').html(qAndAuthorArr[0]);
+        $('#author').html(qAndAuthorArr[1]);
+    }
+
+
